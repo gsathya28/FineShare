30840ecd7236844ee1bba9f06c164672c850be8f
gsathya28 <gsathya28@gmail.com>
2016-11-17 17:14:33 -0600

    Location Path Changes
    changed the parameter for the header() function - Location

--- insidebox/sess/sessqueries.php
+++ insidebox/sess/sessqueries.php
@@ -2,26 +2,26 @@
 	session_start();
 	if (!isset($_SESSION['user']))
 	{
-		header("Location:http://fineshare-gsathya28679192.codeanyapp.com/FineShare/outsidebox/login.php");	
+		header("Location:login.php");
 	}
-	
+
 	// UserInfo
 	$username = $_SESSION['user'];
 	$email = $_SESSION['email'];
 	$type = $_SESSION['type'];
-	
+
 	$userquery = "SELECT name, lastname, type FROM usertable WHERE username = '$username';";
 	$userinfo = $connection->query($userquery);
-	
+
 	if (!$userinfo) die ($connection->error);
-	
+
 	$userarray = $userinfo->fetch_array(MYSQL_ASSOC);
-	
+
 	$firstname = $userarray['name'];
 	$lastname = $userarray['lastname'];
 	$userinfo -> close();
-	
-	
-	
+
+
+
 
 ?>
